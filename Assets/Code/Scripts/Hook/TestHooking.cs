322f86b8e31bdb357a057fab5e958f4d07154001
지현 <88700503+wlgus123@users.noreply.github.com>
2026-02-24 03:58:33 +0900

    [feat] 전투 스테이지 생성
    ref: #9

--- Assets/Code/Scripts/Hook/TestHooking.cs
+++ Assets/Code/Scripts/Hook/TestHooking.cs
@@ -106,7 +106,6 @@ public class TestHooking : MonoBehaviour
             ropePos[i] = hookSegments[i].CurrPos;
 
         line.SetPositions(ropePos);
-		line.sortingOrder = 100;
 		line.gameObject.layer = 15;
     }
 
